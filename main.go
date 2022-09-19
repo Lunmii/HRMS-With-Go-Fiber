85bf0500d77c17aacf81ff8550c54de4e769c65a
Alo Oluwapelumi David <pelumidavid123456@gmail.com>
2022-09-19 17:33:36 +0100

    commit

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"context"
 	"github.com/gofiber/fiber/v2"
+	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
 	"log"
 	"time"
@@ -18,11 +19,12 @@ var mg MongoInstance
 const dbName = "fiber-hrms"
 const mongoURI = "mongodb+srv://lumi:<password>@cluster0.yl6aswd.mongodb.net/?retryWrites=true&w=majority" + dbName
 
+// connect using MongoDB URI or local host
 type Employee struct {
-	ID     string
-	Name   string
-	Salary float64
-	Age    float64
+	ID     string  `json:"id,omitempty" bson:"_id,omitempty"`
+	Name   string  `json:"name"`
+	Salary float64 `json:"salary"`
+	Age    float64 `json:"age"`
 }
 
 func Connect() error {
@@ -50,6 +52,19 @@ func main() {
 	}
 	app := fiber.New()
 	app.Get("/employee", func(c *fiber.Ctx) error {
+
+		query := bson.D{{}}
+
+		cursor, err := mg.Db.Collection("employees").Find(c.Context(), query)
+		if err != nil {
+			return c.Status(500).SendString(err.Error())
+		}
+		var employees []Employee = make([]Employee, 0)
+
+		if err := cursor.All(c.Context(), &employees); err != nil {
+			return c.Status(500).SendString(err.Error())
+		}
+		return c.JSON(employees)
 	})
 
 	app.Post("/employee")
